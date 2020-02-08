15f68f3bc17dc027fe55e10b0d8be2b292966ea3
BrandonAP <barreguin842@gmail.com>
2020-02-08 15:42:16 -0600

    personal portfolio

---
+++ javascript-jquery-files/javascript-documents/script-mobile-menu.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.toggle-burger-button').click(function(){
+        $('.all-navigation-bars-container').toggleClass('active');
+        $('.toggle-burger-button').toggleClass('toggle');
+    })
+})
